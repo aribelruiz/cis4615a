b0bd1d57b8e07ee4d938bb629361cdaf4f9b06d9
aribelruiz <94211795+aribelruiz@users.noreply.github.com>
2022-10-17 14:15:35 -0400

    Update R14_SER01_J.java

--- R14_SER01_J.java
+++ R14_SER01_J.java
@@ -1,18 +1,20 @@
 // Rule 14. Serialization (SER)
 // SER01-J. Do not deviate from the proper signatures of serialization methods.
 
-// Non-compliant code (readObject(), writeObject()): 
+// Compliant code (readObject(), writeObject()): 
 public class Ser implements Serializable {
     private final long serialVersionUID = 123456789;
     private Ser() {
       // Initialize
     }
-    public static void writeObject(final ObjectOutputStream stream)
-      throws IOException {
-      stream.defaultWriteObject();
+
+    private void writeObject(final ObjectOutputStream stream)
+    throws IOException {
+        stream.defaultWriteObject();
     }
-    public static void readObject(final ObjectInputStream stream)
-        throws IOException, ClassNotFoundException {
-      stream.defaultReadObject();
+ 
+    private void readObject(final ObjectInputStream stream)
+    throws IOException, ClassNotFoundException {
+        stream.defaultReadObject();
     }
   }
