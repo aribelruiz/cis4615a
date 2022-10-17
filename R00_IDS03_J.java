da88517bcf7cfcf56608ec400db33752bbc8bdfa
aribelruiz <94211795+aribelruiz@users.noreply.github.com>
2022-10-17 13:27:38 -0400

    Update R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,9 +1,14 @@
 // Rule 00. Input Validation and Data Sanitization (IDS)
 // IDS03-J. Do not log unsanitized user input.
 
-// Non-compliant code:
+// Compliant code:
 if (loginSuccessful) {
-    logger.severe("User login succeeded for: " + username);
-  } else {
-    logger.severe("User login failed for: " + username);
-  }
+  logger.severe("User login succeeded for: " + sanitizeUser(username));
+} else {
+  logger.severe("User login failed for: " + sanitizeUser(username));
+}
+
+public String sanitizeUser(String username) {
+  return Pattern.matches("[A-Za-z0-9_]+", username)
+      ? username : "unauthorized user";
+}
