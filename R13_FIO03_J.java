fa3209724e12fccc1b57009b7346b0416a259a8c
aribelruiz <94211795+aribelruiz@users.noreply.github.com>
2022-10-17 14:10:09 -0400

    Create R13_FIO03_J.java

---
+++ R13_FIO03_J.java
@@ -0,0 +1,28 @@
+// Rule 13. Input Output (FIO)
+// FIO03-J. Remove temporary files before termination.
+
+// Non-compliant code (createTempFile(), deleteOnExit()):
+class TempFile {
+    public static void main(String[] args) throws IOException{
+      File f = File.createTempFile("tempnam",".tmp");
+      FileOutputStream fop = null;
+      try {
+        fop = new FileOutputStream(f);
+        String str = "Data";
+        fop.write(str.getBytes());
+        fop.flush();
+      } finally {
+        // Stream/file still open; file will
+        // not be deleted on Windows systems
+        f.deleteOnExit(); // Delete the file when the JVM terminates
+   
+        if (fop != null) {
+          try {
+            fop.close();
+          } catch (IOException x) {
+            // Handle error
+          }
+        }
+      }
+    }
+  }
