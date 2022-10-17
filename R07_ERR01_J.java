7dd3204acf150530fd6dba864d5f9221e1a0c5fb
aribelruiz <94211795+aribelruiz@users.noreply.github.com>
2022-10-17 13:59:47 -0400

    Update R07_ERR01_J.java

--- R07_ERR01_J.java
+++ R07_ERR01_J.java
@@ -1,7 +1,7 @@
 // Rule 07. Exceptional Behavior (ERR)
 // ERR01-J. Do not allow exceptions to expose sensitive information.
 
-// Non-compliant code:
+// Compliant code:
 class ExceptionExample {
     public static void main(String[] args) {
    
